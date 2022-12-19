94e8074e7a1b03d74b80e78880c19193c6d10b8b
theblockchainarborist <theblockchainarborist@gmail.com>
2022-12-19 14:07:34 -0500

    Start of UI side of Social Trading app

---
+++ my_superalgos_social-trading-app/src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomeView from '../views/HomeView.vue'
+import DevelopersView from '../views/DevelopersView.vue'
+import BitcoinFactoryView from '../views/BitcoinFactoryView.vue'
+import SocialTradingView from '../views/SocialTradingView.vue'
+import TestView from '../views/TestView.vue'
+import ProfileView from '../views/ProfileView.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/social-trading',
+    name: 'Social-Trading',
+    component: SocialTradingView
+  },
+  {
+    path: '/Developers',
+    name: 'Developers',
+    component: DevelopersView
+  },
+  {
+    path: '/BitcoinFactory',
+    name: 'BitcoinFactory',
+    component: BitcoinFactoryView
+  },
+  {
+    path: '/testview',
+    name: 'TestView',
+    component: TestView
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ my_superalgos_social-trading-app/src/views/TestView.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="background-div">
+    <div class="container">
+  <div class="sliding-background">
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+.container {
+    position: relative;
+    overflow:hidden;
+}
+
+.sliding-background {
+    position: fixed;
+    background: url('../assets/homepagebackground.jpg') repeat-x 0 / 100% auto;
+    top: 1%;
+    left: 0%;
+    width: 100vw;
+    height: 100vh;
+    animation: slide 60s linear infinite;
+}
+
+@keyframes slide {
+    0%{background-position: 0 0;}
+	100%{background-position: 100vw 0;}
+}
+
+
+</style>
